61dbfcb421d853d15d11cbfcc45c7e287d2dabfb
Nikhil <nikhilkalakkara05@gmail.com>
2023-11-13 15:18:35 +0530

    FrontEnd COmpleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import {Routes,Route, useNavigate} from 'react-router-dom'
 import './App.css';
+import Authentication from './Components/Authentication/Authentication';
+import HomePage from './Components/HomePage/HomePage';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserProfile } from './Store/Auth/Action';
 
 function App() {
+  const jwt=localStorage.getItem("jwt")
+  const {auth}=useSelector(store=>store)
+  const dispatch=useDispatch()
+  const navigate=useNavigate()
+  
+
+
+  useEffect(()=>{
+
+    if(jwt)
+    {
+      dispatch(getUserProfile(jwt))
+      navigate("/")
+    }
+
+
+
+  },[auth.jwt])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
+      <Routes>
+        <Route path="/*" element={auth.user?<HomePage/> : <Authentication/>}>
+
+        </Route>
+
+      </Routes>
+      
     </div>
   );
 }
